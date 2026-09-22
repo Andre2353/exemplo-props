7e993eddce5c45bc5e31d2eb9a877c6245b34885
Andre2353 <andrepassosdossantos64@gmail.com>
2026-09-22 10:52:26 -0300

    feat: AppStyles.js

---
+++ src/styles/AppStyles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Appcontainer = styled.div`
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+`
+export const GreetingHeader = styled.p`
+    font-size: 1.2rem;
+    color: #555;
+    margin-bottom: 20px;
+    font-weight: 500px;
+
+`
