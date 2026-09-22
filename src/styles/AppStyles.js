d84c472c4f1eb9c681af337818003bf7c1bb010a
Andre2353 <andrepassosdossantos64@gmail.com>
2026-09-22 10:57:45 -0300

    feat: AppStyles.js atualizção

--- src/styles/AppStyles.js
+++ src/styles/AppStyles.js
@@ -14,4 +14,22 @@ export const GreetingHeader = styled.p`
     margin-bottom: 20px;
     font-weight: 500px;
 
+`;
+export const MainTitle = styled.h2`
+font-size: 1.3rem;
+color: #333;
+margin-top: 25px;
+margin-bottom: 10px;
+`;
+
+export const objectiveList = styled.ul`
+list-style-type: disc;
+padding-left: 20px;
+color: #2d3748;
+`;
+
+export const objectiveItem = styled.li`
+font-size: 1rem;
+margin-bottom: 10px;
+font-weight: 500;
 `
